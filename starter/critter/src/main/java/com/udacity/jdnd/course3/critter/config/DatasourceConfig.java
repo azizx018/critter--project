4c8250e8d4680e5dd3ad7f04f2b6571f2c705da9
Aziz, Chiara <chiara_aziz@optum.com>
2022-10-11 13:57:53 -0500

    edited applications file for db config and added config class- application starts successfully

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/config/DatasourceConfig.java
@@ -0,0 +1,23 @@
+package com.udacity.jdnd.course3.critter.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatasourceConfig {
+    @Bean
+    public DataSource getDatasource() {
+        DataSourceBuilder dsb = DataSourceBuilder.create();
+        dsb.username("admin");
+        dsb.password(securePasswordService());
+        dsb.url("jdbc:mysql://localhost:3306/critterdb");
+        return dsb.build();
+    }
+
+    private String securePasswordService() {
+        return "pass963";
+    }
+}
