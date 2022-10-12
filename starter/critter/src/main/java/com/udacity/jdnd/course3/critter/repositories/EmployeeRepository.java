9709232454405d5992c5f02372ff7bdeeb0c61b3
Aziz, Chiara <chiara_aziz@optum.com>
2022-10-12 14:53:03 -0500

    changed to extend by jpa repo

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repositories/EmployeeRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repositories/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.udacity.jdnd.course3.critter.repositories;
 
 import com.udacity.jdnd.course3.critter.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,6 @@ import java.time.DayOfWeek;
 import java.util.List;
 
 @Repository
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     List<Employee> findAllByDaysAvailable(DayOfWeek day);
 }
