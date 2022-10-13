f3efc49a197745f8d6546a1571231b1b19972602
Aziz, Chiara <chiara_aziz@optum.com>
2022-10-13 16:08:48 -0500

    added getcustomerby id method with optional

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/service/CustomerService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/service/CustomerService.java
@@ -4,11 +4,13 @@ import com.udacity.jdnd.course3.critter.entity.Customer;
 import com.udacity.jdnd.course3.critter.entity.Pet;
 import com.udacity.jdnd.course3.critter.repositories.CustomerRepository;
 import com.udacity.jdnd.course3.critter.repositories.PetRepository;
+import org.checkerframework.checker.nullness.Opt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -35,8 +37,12 @@ public class CustomerService {
         return selectedCustomer;
 
     }
-    public void saveCustomer(Customer customer) {
-        customerRepository.save(customer);
+    public Customer saveCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+    public Customer getCustomerById(Long id) {
+        Optional<Customer> optionalCustomer = customerRepository.findById(id);
+        return optionalCustomer.orElse(null);
     }
 
 
