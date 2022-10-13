29228b192b7df9f0c96e1cee67f5e6a7403e4d64
Aziz, Chiara <chiara_aziz@optum.com>
2022-10-13 15:08:33 -0500

    added conversion from pet to DTO and visa versa

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetController.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetController.java
@@ -1,8 +1,14 @@
 package com.udacity.jdnd.course3.critter.pet;
 
+import com.udacity.jdnd.course3.critter.entity.Customer;
+import com.udacity.jdnd.course3.critter.entity.Pet;
+import com.udacity.jdnd.course3.critter.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.beans.BeanUtils.copyProperties;
 
 /**
  * Handles web requests related to Pets.
@@ -10,9 +16,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/pet")
 public class PetController {
+    @Autowired
+    private CustomerService customerService;
 
     @PostMapping
     public PetDTO savePet(@RequestBody PetDTO petDTO) {
+//        Customer customer = null;
+//        if ((Long) petDTO.getOwnerId() != null){
+//            customer = customerService.findByCu
+//        }
         throw new UnsupportedOperationException();
     }
 
@@ -30,4 +42,17 @@ public class PetController {
     public List<PetDTO> getPetsByOwner(@PathVariable long ownerId) {
         throw new UnsupportedOperationException();
     }
+
+//    private PetDTO convertPetToPetDTO(Pet pet) {
+//        PetDTO petDTO = new PetDTO();
+//        copyProperties(pet, petDTO);
+//        petDTO.setOwnerId(pet.getCustomer().getId());
+//        return petDTO;
+//    }
+
+//    private Pet convertPetDTOtoPet(PetDTO petDTO) {
+//        Pet pet = new Pet();
+//        copyProperties(petDTO, pet);
+//        return pet;
+//    }
 }
