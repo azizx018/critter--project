5c2f33303b8560fbef836477dd0f8206d49f225e
Aziz, Chiara <chiara_aziz@optum.com>
2022-10-13 16:08:15 -0500

    added @transactional tag

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repositories/CustomerRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/repositories/CustomerRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
+@Transactional
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
