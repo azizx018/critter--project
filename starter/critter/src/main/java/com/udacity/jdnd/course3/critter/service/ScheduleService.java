1966cff5ef1084b67935e1decf8151dee4573f14
Aziz, Chiara <chiara_aziz@optum.com>
2022-10-19 09:31:44 -0500

    application works and all tests pass and code reivew feedback has been incorporated

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/service/ScheduleService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/service/ScheduleService.java
@@ -1,18 +1,13 @@
 package com.udacity.jdnd.course3.critter.service;
-import com.udacity.jdnd.course3.critter.entity.Customer;
 import com.udacity.jdnd.course3.critter.entity.Pet;
 import com.udacity.jdnd.course3.critter.entity.Schedule;
-import com.udacity.jdnd.course3.critter.repositories.CustomerRepository;
 import com.udacity.jdnd.course3.critter.repositories.EmployeeRepository;
 import com.udacity.jdnd.course3.critter.repositories.PetRepository;
 import com.udacity.jdnd.course3.critter.repositories.ScheduleRepository;
-import com.udacity.jdnd.course3.critter.schedule.ScheduleDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
